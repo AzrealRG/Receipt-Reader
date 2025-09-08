ec8de0bf8c4777d6e985b1510ca198caa0515999
AzrealRG <john.ha2006@gmail.com>
2025-09-08 14:49:14 -0400

    will iterate through files in the recipts folder and checks if they are an image or not and will send it to the api if they are a photo

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import json
 import requests
+import os
 
-url = "https://ocr.asprise.com/api/v1/receipt"
-image = "Recipt1.jpg"
+folder_path = "/Users/johnha/ReciptReader/recipts"
+for filename in os.listdir("recipts"):
+    if filename.lower().endswith(".jpg", ".jpeg", ".png"):
+        file_path = os.path.join(folder_path, filename)
+    else:
+        continue
+    url = "https://ocr.asprise.com/api/v1/receipt"
+    image = file_path
 
-res = requests.post(url,
-                    data = {
-                        'api-key': 'TEST',
-                        'recognizer': 'auto',
-                        'ref_no': 'ocr_python_123'
-                    },
-                    files = {
-                        'file': open(image, 'rb')
-                    })
+    res = requests.post(url,
+                        data = {
+                            'api-key': 'TEST',
+                            'recognizer': 'auto',
+                            'ref_no': 'ocr_python_123'
+                        },
+                        files = {
+                            'file': open(image, 'rb')
+                        })
 
-with open("result1.json", "w") as f:
-    json.dump(json.loads(res.text), f)
+    with open(f"results/{filename}.json", "w") as f:
+        json.dump(json.loads(res.text), f)
+
